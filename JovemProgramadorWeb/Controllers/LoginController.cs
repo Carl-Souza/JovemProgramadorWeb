be06c0fe1cfb792e518de8c95787f97e035a3e37
Carl <carlsagan77@gmail.com>
2025-09-12 21:58:33 -0300

    Adicionar arquivos de projeto.

---
+++ JovemProgramadorWeb/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using JovemProgramadorWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JovemProgramadorWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ValidaUsuario(Usuario usuario)
+        {
+            try
+            {
+                if (usuario.Email == "carlsagan77@gmail.com" && usuario.Senha == "1234")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["MsgErro"] = "Usuário ou senha incorretos! Tente novamente...";
+                }
+            }
+            catch (Exception)
+            {
+                TempData["MsgErro"] = "Usuário ou senha incorretos! Tente novamente...";
+            }
+            return View("Index");
+        }
+    }
+}
